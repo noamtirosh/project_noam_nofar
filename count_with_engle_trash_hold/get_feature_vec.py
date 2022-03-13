3de56e0ba14533c33ceaa586b323416d431fc9da
noamt <noam6tirosh@gmail.com>
2022-03-13 13:53:30 +0200

    change_count code to work with angular velacity

--- count_with_engle_trash_hold/get_feature_vec.py
+++ count_with_engle_trash_hold/get_feature_vec.py
@@ -1,29 +1,51 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-#right side
-right_joint_list = [[13,15,17],[11,13,15],[23,11,13],[25,23,11],[27,25,23], [31,27,25]] #wrist elbow shoulder hip knee ankle
-#left side
-left_joint_list = np.array(right_joint_list) +1
+
+# right side
+left_joint_list = [[13, 15, 17], [11, 13, 15], [23, 11, 13], [25, 23, 11], [27, 25, 23],[31, 27, 25]]  # wrist elbow shoulder hip knee ankle
+# left side
+right_joint_list = np.array(left_joint_list) + 1
 
 
-def get_angles(scelton_points:np.ndarray):
+def get_angles(scelton_points: np.ndarray, right_side: bool):
     # Loop through joint sets
     angles = []
-    for joint in left_joint_list:
+    if right_side:
+        joint_list = right_joint_list
+    else:
+        joint_list = left_joint_list
+    for joint in joint_list:
         a = np.array([scelton_points[joint[0]].x, scelton_points[joint[0]].y])  # First coord
         b = np.array([scelton_points[joint[1]].x, scelton_points[joint[1]].y])  # Second coord
         c = np.array([scelton_points[joint[2]].x, scelton_points[joint[2]].y])  # Third coord
         radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
         angle = np.abs(radians * 180.0 / np.pi)
-        if angle > 180.0:
-            angle = 360 - angle
+        # if angle > 180.0:
+        #     angle = 360 - angle
         angles.append(angle)
     return angles
 
 
-def get_angular_volcity(angels:np.ndarray,time):
-    # Loop through joint sets
-    angle_delta = angels[-1] -angels[0]
-    return angle_delta/(time[-1]-time[-0])
+def get_min_visability(scelton_points: np.ndarray, right_side: bool):
+    if right_side:
+        joint_list = right_joint_list
+    else:
+        joint_list = left_joint_list
+    visability_vector = []
+    for joint in joint_list:
+        angle_vis = [scelton_points[point].visibility for point in joint]
+        visability_vector.append(min(angle_vis))
+    return visability_vector
 
+
+def choose_side(scelton_points: np.ndarray):
+    right_vis = np.mean(get_min_visability(scelton_points, True))
+    left_vis = np.mean(get_min_visability(scelton_points, False))
+    return right_vis > left_vis
+
+
+def get_angular_volcity(angels: np.ndarray, time):
+    # Loop through joint sets
+    angle_delta = angels[-1] - angels[0]
+    return angle_delta / (time[-1] - time[-0])
--- count_with_engle_trash_hold/main.py
+++ count_with_engle_trash_hold/main.py
@@ -1,13 +1,15 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-
+from get_feature_vec import get_angles , get_angular_volcity ,get_min_visability ,choose_side
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 visibility_threshold = 98
 up_threshold = 170
 down_threshold = 140
-
+n_sequence_direction = 4
+up_direction = np.array([1])
+down_direction = np.array([-1])
 
 def calculate_2d_angles(landmarks, joint_list):
         # Loop through joint sets
@@ -56,15 +58,31 @@ def check_visible(landmark,indexes):
     return sum(landmark[indexes].visibility)
 
 
+def choose_position_by_v(velacity_vector:np.ndarray,direction_vector:np.ndarray,threshold_vector:np.ndarray,last_frames_arr:np.ndarray):
+    if np.abs(velacity_vector)>threshold_vector:
+        add_to_frames = velacity_vector*direction_vector
+    else:
+        add_to_frames = 0
+    for ind ,frame in enumerate(last_frames_arr[1:]):
+        last_frames_arr[ind] = frame
+    last_frames_arr[ind+1] = add_to_frames
+    return not np.any(last_frames_arr <= 0)
 
-#video_path = r""
-#cap =cv2.VideoCapture(video_path)
-cap = cv2.VideoCapture(r"C:\Users\noam\Downloads\VID_20211128_164022.mp4")
+
+video_path = r"C:\Users\noam\Downloads\VID_20211128_163923 (1).mp4"
+cap =cv2.VideoCapture(video_path)
+#cap = cv2.VideoCapture(r"C:\Users\noam\Downloads\VID_20211128_164022.mp4")
 
 # Curl counter variables
 counter = 0
 stage = None
-
+last_frame_angles = 0
+last_frame_time = 0
+right_side = False
+last_frames_arr = np.zeros(n_sequence_direction)
+v_angle_direction =up_direction
+v_angle_threshold =np.array([1])
+stage = "down"
 ## Setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
@@ -84,39 +102,34 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         # Extract landmarks
         try:
             landmarks = results.pose_landmarks.landmark
+            if counter == 0:
+                right_side = choose_side(landmarks)
+            angels = get_angles(landmarks, right_side)
+            if not last_frame_angles:
+                last_frame_angles = angels
+            else:
+                volacity_in = np.array([last_frame_angles,angels])
+                last_frame_angles = angels
+                angle_v = get_angular_volcity(volacity_in,[0,1])
+                if choose_position_by_v([angle_v[3]],v_angle_direction,v_angle_threshold,last_frames_arr):
+                    if stage == "down":
+                        v_angle_direction = down_direction
+                        last_frames_arr = np.zeros(n_sequence_direction)
+                        stage = "up"
+                    elif stage == "up":
+                        v_angle_direction = up_direction
+                        stage = "down"
+                        counter += 1
+                        last_frames_arr = np.zeros(n_sequence_direction)
+                        print(counter)
 
-            # Get coordinates
-            hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP .value].x,
-                        landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y,
-                        landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].z]
-            knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
-                     landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y,
-                     landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].z]
-            shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
-                     landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y,
-                     landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z]
-
-            visibility = (landmarks[mp_pose.PoseLandmark.RIGHT_HIP .value].visibility + \
-                         landmarks[mp_pose.PoseLandmark.RIGHT_KNEE .value].visibility + \
-                         landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER .value].visibility)*100/3
-            # Calculate angle
-            angle = calculate_2d_angle(knee, hip, shoulder)
 
             # Visualize angle
-            cv2.putText(image, str(angle),
-                        tuple(np.multiply(hip[0:2], [640, 480]).astype(int)),
+            cv2.putText(image, str(angle_v[3]),
+                        tuple(np.multiply([0.5,0.5], [640, 480]).astype(int)),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
                         )
 
-            # Curl counter logic
-            if visibility > visibility_threshold:
-                rec_color = (117, 245, 16)
-                if angle > up_threshold:
-                    stage = "up"
-                if angle < down_threshold and stage == 'up':
-                    stage = "down"
-                    counter += 1
-                    print(counter)
 
         except:
             visibility = 0
@@ -153,4 +166,6 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
     cap.release()
     cv2.destroyAllWindows()
+
+
 
