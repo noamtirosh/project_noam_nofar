d63ac5d54290040f3359263a80aff3b9670e3dbd
noamt <noam6tirosh@gmail.com>
2022-04-18 10:10:19 +0300

    update tools menual

--- tools/save_to_csv.py
+++ tools/save_to_csv.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 
 
-def export_landmarks(scelton_points: np.ndarray, action):
+def export_landmarks(file_name: str, scelton_points: np.ndarray, action):
     try:
         kay_points = np.array(
             [[point.x, point.y, point.z, point.visibility] for point in scelton_points]).flatten()
@@ -12,18 +12,20 @@ def export_landmarks(scelton_points: np.ndarray, action):
         row.insert(0, action)
         # kay_points = [action,0,kay_points]
         # kay_points.insert(0,action)
-        with open("coords.csv", mode='a', newline='') as f:
+        with open(file_name, mode='a', newline='') as f:
             csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             csv_writer.writerow(row)
     except Exception as e:
         pass
 
 
-def create_first_row():
+def create_first_row(csv_name: str = "data.csv", over_write: bool = False):
+    if not over_write and os.path.isfile(csv_name):
+        return
     # init csv_file
     landmarks = ["class"]
     for val in range(1, 33 + 1):
         landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
-    with open("coords.csv", mode='w', newline='') as f:
+    with open(csv_name, mode='w', newline='') as f:
         csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow(landmarks)
