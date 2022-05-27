671e29f29047f0653dab06395a39bc40a17580ca
noamt <noam6tirosh@gmail.com>
2022-05-27 19:25:24 +0300

    fix_bug in manual_create

--- create_train_set/manual_create.py
+++ create_train_set/manual_create.py
@@ -21,66 +21,72 @@ def manual_create(video_list: list, csv_file_name: str, run_video: bool = False)
 
     for video in video_list:
         cap = cv2.VideoCapture(video)
-        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-            while cap.isOpened():
-                ret, frame = cap.read()
-                if (type(frame) != np.ndarray):
-                    break
-                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                image.flags.writeable = False
-                # Make detection
-                # check if contain r\l for side
-                if Path(video).name.lower().count("r") or Path(video).name.lower().count("right"):
-                    right_side = image
-                    left_side = cv2.flip(image, 1)
-                elif Path(video).name.lower().count("l") or Path(video).name.lower().count("left"):
-                    left_side = image
-                    right_side = cv2.flip(image, 1)
-                else:
-                    print("the file:{} not have side ".format(video))
-                right_res = pose.process(right_side)
-                left_res = pose.process(left_side)
-                # Recolor back to BGR
-                image.flags.writeable = True
-                image = cv2.cvtColor(right_side, cv2.COLOR_RGB2BGR)
-                try:
-                    right_landmarks = right_res.pose_landmarks.landmark
-                    left_landmarks = left_res.pose_landmarks.landmark
+        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose_r:
+            with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose_l:
+                while cap.isOpened():
+                    ret, frame = cap.read()
+                    if (type(frame) != np.ndarray):
+                        break
+                    # Make detection
+                    # check if contain r\l for side
+                    if Path(video).name.lower().count("right"):
+                        right_side_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                        left_side_image = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
+                        right_side_image.flags.writeable = False
+                        left_side_image.flags.writeable = False
+                    elif Path(video).name.lower().count("left"):
+                        left_side_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                        right_side_image = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
+                        right_side_image.flags.writeable = False
+                        left_side_image.flags.writeable = False
 
-                    # cv2.imshow('Mediapipe Feed', image)
-                    mp_drawing.draw_landmarks(image, right_res.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2,
-                                                                     circle_radius=2),
-                                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2,
-                                                                     circle_radius=2)
-                                              )
-                    imS = cv2.resize(image, (960, 540))  # Resize image
-                    cv2.imshow('Mediapipe Feed', imS)
-                    k = cv2.waitKey(10)
-                    while k != ord('n'):
-                        if k == ord('u'):
-                            export_landmarks(right_csv_name, right_landmarks, 'up')
-                            export_landmarks(left_csv_name, left_landmarks, 'up')
-                            break
-                        if k == ord('d'):
-                            export_landmarks(right_csv_name, right_landmarks, 'douwn')
-                            export_landmarks(left_csv_name, left_landmarks, 'douwn')
-                            break
-                        if k == ord('m'):
-                            export_landmarks(right_csv_name, right_landmarks, 'middle')
-                            export_landmarks(left_csv_name, left_landmarks, 'middle')
-                            break
-                        if k == ord('q'):
-                            exit()
-                        if run_video:
-                            break
+                    else:
+                        print("the file:{} not have side ".format(video))
+                    # right_res = pose.process(right_side_image)
+                    left_res = pose_l.process(left_side_image)
+                    right_res = pose_r.process(right_side_image)
+                    # Recolor back to BGR
+                    right_side_image = cv2.cvtColor(right_side_image, cv2.COLOR_RGB2BGR)
+                    right_side_image.flags.writeable = True
+
+                    try:
+                        right_landmarks = right_res.pose_landmarks.landmark
+                        left_landmarks = left_res.pose_landmarks.landmark
+
+                        # cv2.imshow('Mediapipe Feed', image)
+                        mp_drawing.draw_landmarks(right_side_image, right_res.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2,
+                                                                         circle_radius=2),
+                                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2,
+                                                                         circle_radius=2)
+                                                  )
+                        imS = cv2.resize(right_side_image, (960, 540))  # Resize image
+                        cv2.imshow('Mediapipe Feed', imS)
                         k = cv2.waitKey(10)
-                except:
-                    visibility = 0
-                    pass
+                        while k != ord('n'):
+                            if k == ord('u'):
+                                export_landmarks(right_csv_name, right_landmarks, 'up')
+                                export_landmarks(left_csv_name, left_landmarks, 'up')
+                                break
+                            if k == ord('d'):
+                                export_landmarks(right_csv_name, right_landmarks, 'douwn')
+                                export_landmarks(left_csv_name, left_landmarks, 'douwn')
+                                break
+                            if k == ord('m'):
+                                export_landmarks(right_csv_name, right_landmarks, 'middle')
+                                export_landmarks(left_csv_name, left_landmarks, 'middle')
+                                break
+                            if k == ord('q'):
+                                exit()
+                            if run_video:
+                                break
+                            k = cv2.waitKey(10)
+                    except:
+                        visibility = 0
+                        pass
 
-                if cv2.waitKey(10) & 0xFF == ord('q'):
-                    break
+                    if cv2.waitKey(10) & 0xFF == ord('q'):
+                        break
 
             cap.release()
             cv2.destroyAllWindows()
@@ -90,4 +96,5 @@ if __name__ == '__main__':
 
     folder_to_use = r"C:\Users\noam\Videos\project\currect"
     video_list = glob.glob(folder_to_use+r"\*.mp4")
+    video_list =[r"C:\Users\noam\Downloads\right.mp4"]
     manual_create(video_list, folder_to_use +r"\data1.csv", False)
